6e1b6778495aa11fe82df303f3eb93ce94d35c5b
Harold Calayan <haroldcalayan@gmail.com>
2021-07-08 00:40:14 +0800

    :sparkles: Create application class

---
+++ app/src/main/java/com/haroldcalayan/calculator/CalculatorApplication.kt
@@ -0,0 +1,18 @@
+package com.haroldcalayan.calculator
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class CalculatorApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+}
